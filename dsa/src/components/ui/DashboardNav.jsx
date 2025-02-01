d4a82e6e0f1ef7411644f481e200e4dc4ce5f3ca
aniket-paraswar <aniket.paraswar23@spit.ac.in>
2025-02-01 14:52:22 +0530

    add

--- dsa/src/components/ui/DashboardNav.jsx
+++ dsa/src/components/ui/DashboardNav.jsx
@@ -37,7 +37,7 @@ const DashboardNav = ({ onMenuClick }) => {
             { label: "Home", icon: <FaHome className="text-blue-400" />, section: "home" },
             { label: "Messages", icon: <FaEnvelope className="text-red-400" />, section: "messages" },
             { label: "Global", icon: <FaTrophy className="text-yellow-400" />, section: "global" },
-            { label: "Challenge", icon: <FaShieldAlt className="text-green-400" />, section: "challenge" },
+            { label: "Visualize", icon: <FaShieldAlt className="text-green-400" />, section: "challenge" },
             { label: "Discuss", icon: <FaBullseye className="text-purple-400" />, section: "goals" },
             { label: "DCC", icon: <FaCalendarCheck className="text-orange-400" />, section: "dcc" },
             { label: "Sheets", icon: <FaGraduationCap className="text-indigo-400" />, section: "sheets" },
