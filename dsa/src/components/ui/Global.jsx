2fc2d7337ea0f387e62087306efc4e284e9fc94f
aniket-paraswar <aniket.paraswar23@spit.ac.in>
2025-02-01 13:31:45 +0530

    dep

--- dsa/src/components/ui/Global.jsx
+++ dsa/src/components/ui/Global.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Spline from "@splinetool/react-spline";
+
 
 const Global = () => {
   return (
-    <div>Global</div>
+    <main>
+      <Spline
+        scene="https://prod.spline.design/rS73aLkh6O1-ZvUa/scene.splinecode"
+      />
+    </main>
   )
 }
 
